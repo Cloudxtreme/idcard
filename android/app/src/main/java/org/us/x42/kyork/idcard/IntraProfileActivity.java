cc3a5606bcd05e595ef277e8c6faf6982c1464c7
Andy <AndyShih51@gmail.com>
2018-07-19 01:06:13 -0700

    IntraProfileActivity.java udpated, still need OAuth2

--- android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
+++ android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
@@ -1,19 +1,20 @@
 package org.us.x42.kyork.idcard;
 
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
-import java.net.URL;
+
 
 public class IntraProfileActivity extends AppCompatActivity {
 
@@ -25,22 +26,13 @@ public class IntraProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent launchIntent = getIntent();
-        String profileName = launchIntent.getStringExtra("login");
+        String login = launchIntent.getStringExtra("login");
+        login = "apuel";
 
         ImageView bmImage = findViewById(R.id.user_picture);
+        Picasso.get().load("https://cdn.intra.42.fr/users/medium_" + login + ".jpg").into(bmImage);
 
-        Picasso.get().load("https://cdn.intra.42.fr/users/medium_apuel.jpg").into(bmImage);
-        // can't gradle
-
-//        Glide.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
-        // can't gradle
-
-
-//        try {
-//            bmImage.setImageBitmap(BitmapFactory.decodeStream((new URL("http://i.imgur.com/DvpvklR.png")).openConnection() .getInputStream()));
-//        } catch (IOException e) {
-//
-//        }
+        // oauth2 wat do????
 
 
 
@@ -49,16 +41,80 @@ public class IntraProfileActivity extends AppCompatActivity {
 
 
 
+        TextView fullNameText = (TextView)findViewById(R.id.full_name);
+        fullNameText.setText(getFullName(login));
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        TextView loginText = (TextView)findViewById(R.id.login);
+        loginText.setText(getLogin(login));
 
+        TextView titleText = (TextView)findViewById(R.id.title);
+        titleText.setText(getTitle(login));
+
+        TextView levelText = (TextView)findViewById(R.id.level);
+        levelText.setText(getLevel(login));
+
+        TextView gradeText = (TextView)findViewById(R.id.grade);
+        gradeText.setText(getGrade(login));
+
+        TextView accountTypeText = (TextView)findViewById(R.id.account_type);
+        accountTypeText.setText(getAccountType(login));
+
+        TextView coalitionText = (TextView)findViewById(R.id.coalition);
+        coalitionText.setText(getCoalition(login));
+
+        TextView phoneText = (TextView)findViewById(R.id.phone);
+        phoneText.setText(getPhone(login));
     }
+
+    private String getFullName(String login)
+    {
+        return "Andres Puel";
+    }
+
+    private String getLogin(String login)
+    {
+        return "Login: " + login;
+    }
+
+    private String getTitle(String login)
+    {
+        return "Title: " + "emulat0r";
+    }
+
+    private String getLevel(String login)
+    {
+        return "Level: " + 9001;
+    }
+
+    private String getGrade(String login)
+    {
+        return "Grade: " + "High Lieutenant, Jr.";
+    }
+
+    private String getAccountType(String login)
+    {
+        return "Type: " + "Non-b00calian";
+    }
+
+    private String getCoalition(String login)
+    {
+        return "Coalition: " + "Memel0rds";
+    }
+
+    private String getPhone(String login)
+    {
+        return "Phone: " + "1-800-NID-FOOD";
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }

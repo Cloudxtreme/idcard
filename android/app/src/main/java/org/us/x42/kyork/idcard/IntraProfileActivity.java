1590036e569a8417465221e779ccd5f3d01511c6
Maurice Sharpe <msharpe@e1z1r8p20.42.us.org>
2018-07-19 18:11:34 -0700

    Menu modding

--- android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
+++ android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
@@ -25,6 +25,30 @@ public class IntraProfileActivity extends AppCompatActivity {
     private static IntraAPI api = null;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_setup, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+    @Override
     protected void onActivityResult (int requestCode, int resultCode, Intent data) {
 
     }

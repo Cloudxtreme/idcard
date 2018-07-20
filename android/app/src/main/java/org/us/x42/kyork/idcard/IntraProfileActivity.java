c81ee70e126d70f9de5c95d51cb93d92d43d4ae3
Kane York <kyork@student.42.us.org>
2018-07-19 19:13:08 -0700

    fix compile: import Menu

--- android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
+++ android/app/src/main/java/org/us/x42/kyork/idcard/IntraProfileActivity.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
